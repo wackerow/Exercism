a3cf2f722d94245d2c7ca6ef695e46595b776430
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2020-11-06 21:52:53 -0800

    Solved luhn
    Refactored

--- typescript/luhn/luhn.ts
+++ typescript/luhn/luhn.ts
@@ -1,25 +1,36 @@
 export default class Luhn {
   public static valid(_numbers: string): boolean {
+    // Sanitize and remove all spaces
     const numbers = _numbers.replace(/\s/g, '');
+    // If too short, or contains anything other than numbers, return false
     if (
       numbers.length < 2 ||
       numbers.match(/[^0-9]/g)
     ) {
       return false;
     }
+    // Split string into array
     const stringArray = numbers.split('');
+    // Initiate new empty array of type number
     const numArray: number[] = [];
-    for (const num of stringArray) {
-      numArray.push(+num)
-    }
-    for (let i = 0; i < numArray.length; i++) {
-      if ((i + numArray.length) % 2 === 0) {
-        numArray[i] = numArray[i] * 2 > 9 ?
-          numArray[i] * 2 - 9 :
-          numArray[i] * 2;
+    // Iterate over each "number", convert to number type
+    for (let i = 0; i < stringArray.length; i++) {
+      let nextNumber: number;
+      // Every other position gets doubled and reduced to single digit
+      if ((i + stringArray.length) % 2 === 0) {
+        nextNumber = +stringArray[i] * 2 > 9 ?
+          +stringArray[i] * 2 - 9 :
+          +stringArray[i] * 2;
+      } else {
+        // Otherwise just add integer form of number
+        nextNumber = +stringArray[i];
       }
+      // Push to array of numbers
+      numArray.push(nextNumber);
     }
+    // Checksum is last number
     const checksum = numArray[numArray.length - 1];
+    // Return if Luhn logic matches checksum
     return (
         numArray
         .slice(0, numArray.length - 1)
