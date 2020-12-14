8f5e71e3926437a44256da333fb2a9b06ec40f8c
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2020-12-13 19:35:57 -0800

    Solved flatten-array

---
+++ typescript/flatten-array/flatten-array.ts
@@ -0,0 +1,15 @@
+type ComplexArray = Array<number | null | undefined | ComplexArray>
+
+export default class FlattenArray {
+  public static flatten(_array: ComplexArray): number[] {
+    const flattened: number[] = []
+    for (const item of _array) {
+      if (typeof item === "number") {
+        flattened.push(item)
+      } else if (typeof item === "object") {
+        flattened.push(...this.flatten(item as number[]))
+      }
+    } 
+    return flattened
+  }
+}
