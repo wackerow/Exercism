79e92ed734f10689e0eaaf14320bfe3e43fbc7c7
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2020-12-17 05:19:58 -0800

    Solved roman-numerals
    version 2

--- typescript/roman-numerals/roman-numerals.ts
+++ typescript/roman-numerals/roman-numerals.ts
@@ -1,68 +1,27 @@
 export default class RomanNumerals {
-   public static roman(base10: number): string {
+  private static keys: {[s: string]: number} = {
+    'M': 1000,
+    'CM': 900,
+    'D': 500,
+    'CD': 400,
+    'C': 100,
+    'XC': 90,
+    'L': 50,
+    'XL': 40,
+    'X': 10,
+    'IX': 9,
+    'V': 5,
+    'IV': 4,
+    'I': 1
+  }
+  
+  public static roman(base10: number): string {
     let workingNumeral = base10
     let romanNumeral = ''
-    // 10^3
-    while (workingNumeral >= 1000) {
-      romanNumeral += 'M'
-      workingNumeral -= 1000
-    }
-    // 10^2
-    if (workingNumeral >= 900) {
-      romanNumeral += 'CM'
-      workingNumeral -= 900
-    } else if (workingNumeral >= 500) {
-      romanNumeral += 'D'
-      workingNumeral -= 500
-      while (workingNumeral >= 100) {
-        romanNumeral += 'C'
-        workingNumeral -= 100
-      }
-    } else if (workingNumeral >= 400) {
-      romanNumeral += 'CD'
-      workingNumeral -= 400      
-    } else if (workingNumeral >= 100) {
-      while (workingNumeral >= 100) {
-        romanNumeral += 'C'
-        workingNumeral -= 100
-      }
-    }
-    // 10^1
-    if (workingNumeral >= 90) {
-      romanNumeral += 'XC'
-      workingNumeral -= 90
-    } else if (workingNumeral >= 50) {
-      romanNumeral += 'L'
-      workingNumeral -= 50
-      while (workingNumeral >= 10) {
-        romanNumeral += 'X'
-        workingNumeral -= 10
-      }
-    } else if (workingNumeral >= 40) {
-      romanNumeral += 'XL'
-      workingNumeral -= 40
-    } else if (workingNumeral >= 10) {
-      while (workingNumeral >= 10) {
-        romanNumeral += 'X'
-        workingNumeral -= 10
-      }
-    }
-    // 10^0
-    if (workingNumeral >= 9) {
-      romanNumeral += 'IX'
-    } else if (workingNumeral >= 5) {
-      romanNumeral += 'V'
-      workingNumeral -= 5
-      while (workingNumeral >= 1) {
-        romanNumeral += 'I'
-        workingNumeral -= 1
-      }
-    } else if (workingNumeral >= 4) {
-      romanNumeral += 'IV'
-    } else if (workingNumeral >= 1) {
-      while (workingNumeral >= 1) {
-        romanNumeral += 'I'
-        workingNumeral -= 1 
+    for (const key of Object.keys(this.keys)) {
+      while (workingNumeral >= this.keys[key]) {
+        romanNumeral += key
+        workingNumeral -= this.keys[key]
       }
     }
     return romanNumeral
